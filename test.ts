b46a28108ed4e7f174c82f5f47a09d1c268fdfdf
WC Leung <lwchkg@gmail.com>
2018-12-24 23:37:12 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
 // tests go here; this will not be compiled when this package is used as a library
 
 // TODO: enable tests (they crash for no reason :-( Bugs in compiler?)
-//proportionalFont.showNumber(1234)
-//proportionalFont.showNumber(1234, 100)
-//proportionalFont.showString("test okay")
-//proportionalFont.showString("test okay", 100)
+proportionalFont.showNumber(1234)
+proportionalFont.showNumber(1234, 100)
+proportionalFont.showString("test okay")
+proportionalFont.showString("test okay", 100)
 proportionalFont.showSpace(2)
 proportionalFont.showSpace(2, 100)
-//basic.showNumber(proportionalFont.getWidthOfNumber(1234))
-//basic.showNumber(proportionalFont.getWidthOfString("test okay"))
+basic.showNumber(proportionalFont.getWidthOfNumber(1234))
+basic.showNumber(proportionalFont.getWidthOfString("test okay"))
